fa6bf29ad587eb088f837e2385955546cf0b9969
Victor Batischev <vitek.batishev97@gmail.com>
2025-06-03 21:11:26 +0300

    dist folder

---
+++ dist/assets/index-C8uWBRnD.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-DuVhgg5d.js";import"./index-C0noLaBH.js";var d=a;export{d as default};
---
+++ dist/assets/src-UW24ZMRV-bbhhtR3d.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-DuVhgg5d.js";import"./index-C0noLaBH.js";var t=a;export{t as default};
